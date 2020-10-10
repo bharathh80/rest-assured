d08aec09645f2a6501d9ec9a98b86ba57170e1b7
bharathh80 <42533134+bharathh80@users.noreply.github.com>
2020-10-10 08:31:14 +0530

    Add files via upload

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+class HelloWorld {
+
+    @Test
+    public void helloWorld() {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/TestAPIs.java
@@ -0,0 +1,13 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class TestAPIs  {
+
+    @Test
+
+    public void testGetUsers() throws NoSuchFieldException, Exception {
+    }
+
+}
+
+
