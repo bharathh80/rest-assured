13e3bdcdee1cb7d57206fb160b57f32a7f2e3d31
bharathh80 <42533134+bharathh80@users.noreply.github.com>
2020-10-10 08:42:09 +0530

    Update TestAPIs.java

--- src/test/java/TestAPIs.java
+++ src/test/java/TestAPIs.java
@@ -1,12 +1,50 @@
-import io.restassured.response.Response;
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 public class TestAPIs  {
+    
 
     @Test
-
     public void testGetUsers() throws NoSuchFieldException, Exception {
+        //TODO: Add your code here
     }
+    
+    
+     /****************************************************************************************************************************************************
+     Function Name : callGet
+     Description : Performs a get with URL, headers and verifies with the status code expected
+     Parameters : URL, headers and status code
+     *****************************************************************************************************************************************************/
+
+    public void callGet(String URL, int statusCode) {
+        System.out.println("-----------------------***GET CALL***-----------------------");
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(URL)
+                .then()
+                .assertThat().statusCode(statusCode).and().extract().response();
+    }
+
+    /****************************************************************************************************************************************************
+    Function Name : callGetWithQueryParam
+    Description : Performs a get with URL, headers and verifies with the status code expected
+    Parameters : URL, headers and status code
+     *****************************************************************************************************************************************************/
+
+    public void callGetWithQueryParam(String URL, int statusCode, String queryKey, String queryValue) {
+        System.out.println("-----------------------***GET CALL WITH PARAMS***-----------------------");
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .queryParam(queryKey,queryValue)
+                .when()
+                .get(URL)
+                .then()
+                .assertThat().statusCode(statusCode).and().extract().response();
+    }
+
 
 }
 
